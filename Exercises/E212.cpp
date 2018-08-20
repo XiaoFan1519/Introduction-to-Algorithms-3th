f5e6ec47f663d838dc25d1b744b5a9b945408ac2
Fan <xiaofanms@outlook.com>
2018-08-20 20:31:47 +0800

    按照新规范进行重写。

---
+++ Exercises/E212.cpp
@@ -0,0 +1,48 @@
+﻿#include "../Exercise.h"
+
+using namespace Algorithms;
+
+// 插入排序
+void InsertSort(std::vector<int> &arr)
+{
+	size_t size = arr.size();
+	for (int i = 1; i < size; i++)
+	{
+		int key = arr[i];
+		int j = i - 1;
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+class E212 : public Algorithms::Exercise
+{
+public:
+	E212() : Exercise(this)
+	{
+	}
+
+	~E212()
+	{
+	}
+
+	const std::string Name() const override
+	{
+		return "2.1-2";
+	}
+	void Execute() const override
+	{
+		int size = 20;
+		std::vector<int> vec = GenerateElement(size);
+		PrintCollection(vec);
+		InsertSort(vec);
+		PrintCollection(vec);
+		std::cout << "插入排序" << std::endl;
+	}
+};
+
+E212 e212;
---
+++ Exercises/E234.cpp
@@ -0,0 +1,51 @@
+﻿#include "../Exercise.h"
+
+using namespace Algorithms;
+
+// 插入排序，递归实现
+void InsertSort_Recursion(std::vector<int> &arr, int size)
+{
+	if (1 == size)
+	{
+		return;
+	}
+
+	int end = size - 1;
+	InsertSort_Recursion(arr, size - 1);
+	int key = arr[end];
+	int j = end;
+	while (j > 0 && arr[j - 1] > key)
+	{
+		arr[j] = arr[j - 1];
+		j--;
+	}
+	arr[j] = key;
+}
+
+class E234 : public Algorithms::Exercise
+{
+public:
+	E234() : Exercise(this)
+	{
+	}
+
+	~E234()
+	{
+	}
+
+	const std::string Name() const override
+	{
+		return "2.3-4";
+	}
+	void Execute() const override
+	{
+		int size = 20;
+		std::vector<int> vec = GenerateElement(size);
+		PrintCollection(vec);
+		InsertSort_Recursion(vec, size);
+		PrintCollection(vec);
+		std::cout << "插入排序递归实现" << std::endl;
+	}
+};
+
+E234 e234;
